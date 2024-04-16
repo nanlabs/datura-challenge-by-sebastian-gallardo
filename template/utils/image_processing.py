e3e179f54d2a270f8cf013c128fb4f64792badbd
Sebastian Gallardo <sebastian.gallardo@nan-labs.com>
2024-04-16 16:31:05 -0300

    Image processing utils

---
+++ template/utils/image_processing.py
@@ -0,0 +1,14 @@
+import os
+import cv2
+import easyocr
+import numpy as np
+
+def load_image(filename: str) -> np.ndarray:
+    full_image_path = os.path.join(os.path.dirname(__file__), 'images', filename)
+    image = cv2.imread(full_image_path)
+    return image
+
+def get_text(image: np.ndarray) -> str:
+    reader = easyocr.Reader(['en'])
+    result = reader.readtext(image, detail = 0)
+    return ' '.join(result)
